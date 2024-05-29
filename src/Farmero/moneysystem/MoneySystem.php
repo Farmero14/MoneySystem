b4a23f26f1185525f2a0da4f560b520836766bb0
Farmero14 <145240944+Farmero14@users.noreply.github.com>
2024-05-29 14:22:58 -0700

    Update MoneySystem.php

--- src/Farmero/moneysystem/MoneySystem.php
+++ src/Farmero/moneysystem/MoneySystem.php
@@ -52,7 +52,7 @@ class MoneySystem extends PluginBase implements Listener {
 
     private function registerEvents() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getPluginManager()->registerEvents($this->economyScoreHud, $this);
+        $this->getServer()->getPluginManager()->registerEvents($this->moneyScoreHud, $this);
     }
 
     public static function getInstance(): self {
